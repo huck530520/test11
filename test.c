ed6e09b7eef91f1f5178564ada34ffaa309f6a72
justin.wu <justin.wu@realtek.com>
2019-09-03 15:32:18 +0800

    gitt practice
    Change-Id: I7bcea8722705f228b5f23d671f20e0fd28ab53e6
    Signed-off-by: justin.wu <justin.wu@realtek.com>

--- test.c
+++ test.c
@@ -5,7 +5,10 @@ int main()
   // robert: add a comment to test git/repo/gerrit
   int not_used; /* declared but not used variable. */
   int a;
+  int b;
+
+  b = (a - 1) << 2;
   printf ("Hello Jenkins\n");
-  printf("%d",a);
+  printf("%d",b);
   return 0;
 }
