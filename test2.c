b5e6bfc81753e0cedb5937d2f7238148a1b96a58
Shun-Yen Lu <shunyen@realtek.com>
2020-07-28 13:09:09 +0800

    test rebase
    Signed-off-by: Shun-Yen Lu <shunyen@realtek.com>
    Change-Id: I633d27280c0a84e6711041a2ecebc86d9d388cec

---
+++ test2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  // robert: add a comment to test git/repo/gerrit
+  int not_used; /* declared but not used variable. */
+  int a;
+  int b, c, d;
+
+  a = 1;
+  b = a + 2;
+  a = b << 2;
+
+  printf ("Hello Jenkins\n");
+  printf ("Hello Jenkins\n");
+  printf ("Hello Jenkins\n");
+  printf("%d",a);
+
+  printf ("Test https\n");
+  printf ("Modified test.c\n");
+  printf ("Modified test.c again\n");
+  return 0;
+}
