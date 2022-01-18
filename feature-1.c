dc256eac769cb0c52413009fdcc2adaa03dfaaf1
Rey Cheng <reycheng@realtek.com>
2022-01-18 17:48:33 +0800

    dollar sign
    Change-Id: I8e5737a0d61a7e2832d1d7a5c6dbb1a7be01b5cd

--- feature-1.c
+++ feature-1.c
@@ -1,6 +1,6 @@
 int main() {
     printf("rtos-3.1.0 beta 0.1");
     printf("rtos-3.1.0 beta 0.2");
-    printf("rtos-3.1.0 beta 0.3 lnx forbidden trigger test final");
+    printf("rtos-3.1.0 beta 0.3 lnx forbidden trigger test");
     retun 0;
 }
