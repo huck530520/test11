84e2735290bc53660ae3a97785eafa429ccd0cb3
Rey Cheng <reycheng@realtek.com>
2022-01-18 15:04:33 +0800

    lnx 2
    Change-Id: Iedba9002cbed03f2df0a9ee0420a6d1c155656a5

--- feature-1.c
+++ feature-1.c
@@ -2,5 +2,6 @@ int main() {
     printf("rtos-3.1.0 beta 0.1");
     printf("rtos-3.1.0 beta 0.2");
     printf("rtos-3.1.0 beta 0.3 lnx forbidden");
+    printf("rtos-3.1.0 beta 0.3 lnx forbidden 2");
     retun 0;
 }
