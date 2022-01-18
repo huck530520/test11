d9ccf6fafbfcb552f99cc59f6906b5bdd7c8c5c6
Rey Cheng <reycheng@realtek.com>
2022-01-18 16:19:37 +0800

    trigger test 2
    Change-Id: I839151fed1fa3ea7094182627a02b6f2de06e23e

--- feature-1.c
+++ feature-1.c
@@ -1,6 +1,6 @@
 int main() {
     printf("rtos-3.1.0 beta 0.1");
     printf("rtos-3.1.0 beta 0.2");
-    printf("rtos-3.1.0 beta 0.3 lnx forbidden trigger test");
+    printf("rtos-3.1.0 beta 0.3 lnx forbidden trigger test 2");
     retun 0;
 }
