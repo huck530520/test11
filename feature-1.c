c079f93aa25df601eaf80b3979ec7b7fa7664f5b
Rey Cheng <reycheng@realtek.com>
2022-01-19 09:43:21 +0800

    staging 2
    Change-Id: I23b6dea41676e412ec72d492dd762d0329902014

--- feature-1.c
+++ feature-1.c
@@ -1,6 +1,6 @@
 int main() {
     printf("rtos-3.1.0 beta 0.1");
     printf("rtos-3.1.0 beta 0.2");
-    printf("rtos-3.1.0 beta 0.3 staging test");
+    printf("rtos-3.1.0 beta 0.3 staging test 2");
     retun 0;
 }
