fa298e8592bc85f909747912b415a8e075a30537
Rey Cheng <reycheng@realtek.com>
2022-01-19 10:04:35 +0800

    release
    Change-Id: I67a58d39b37afba10bf1082db409f0f8e5024361

--- feature-1.c
+++ feature-1.c
@@ -1,6 +1,6 @@
 int main() {
     printf("rtos-3.1.0 beta 0.1");
     printf("rtos-3.1.0 beta 0.2");
-    printf("rtos-3.1.0 beta 0.3 staging test 5");
+    printf("rtos-3.1.0 beta 0.3 release");
     retun 0;
 }
