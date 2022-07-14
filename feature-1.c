980e7116bd48626b62a6a1bf836fb25bc3e29dc4
Rey Cheng <reycheng@realtek.com>
2022-07-14 16:26:48 +0800

    update
    Change-Id: Ibffddd954c7eab529803fd8377bcdeeb6a33245a
    Signed-off-by: Rey Cheng <reycheng@realtek.com>

--- feature-1.c
+++ feature-1.c
@@ -1,9 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef unsigned char   uint8_t;
+
+void out_of_bound_access(int data_len) {
+    static uint8_t *buf = NULL;
+    char data[1024];
+    memset(data, '\0', 1024);
+
+    if (data_len > 100) {
+        buf = malloc(100);
+    }
+
+    memcpy(buf, data, data_len);
+}
+
 int main() {
     int *p;
     *p = 3;
 
     int *p2;
     *p2 = 4;
+
+    out_of_bound_access(2048);
 
     printf("trigger by comment added on gerrit-dup-4\n");
     return 0;
