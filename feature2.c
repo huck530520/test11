b5350e063256fb4ba18ab5456fc8498bc2d08e60
Rey Cheng <reycheng@realtek.com>
2023-03-06 15:56:57 +0800

    test analysis
    Change-Id: I8729b2eaf89196b121663affebcfe942ff34f09b
    Signed-off-by: Rey Cheng <reycheng@realtek.com>

--- feature2.c
+++ feature2.c
@@ -1,10 +1,13 @@
 int main() {
     int *p;
-    *p = 5;
+    //*p = 5;
     free(p);
 
     int *q = malloc(sizeof(int));
     memset(q, 0, sizeof(*q));
+
+    int *r;
+    *r = 1001;
 
     return 0;
 }
