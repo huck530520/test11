7191436df4af16d42b64cb0234b3f4590f6b7e5c
Rey Cheng <reycheng@realtek.com>
2023-03-07 15:00:13 +0800

    test parent
    Change-Id: I79af98cd89b7d75c58386c52ba89675161bee7d0
    Signed-off-by: Rey Cheng <reycheng@realtek.com>

--- feature2.c
+++ feature2.c
@@ -1,17 +1,17 @@
 int main() {
-    //int *p;
-    //*p = 5;
-    //free(p);
+    int *p;
+    *p = 5;
+    free(p);
 
     int *q = malloc(sizeof(int));
     memset(q, 0, sizeof(*q));
 
-    //int *r;
-    //*r = 1001;
+    int *r;
+    *r = 1001;
 
-    int *s;
-    *s = 999;
-    *s = 1000;
+    //int *s;
+    //*s = 999;
+    //*s = 1000;
 
     return 0;
 }
