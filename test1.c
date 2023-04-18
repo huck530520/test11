8007aa1117b6124fc3119bdfdee2273aa8046e2d
Jun-Fei Cherng <jfcherng@realtek.com>
2023-04-18 15:11:52 +0800

    xd
    Change-Id: I5e9b1610353aeb6e2c9f5037e0da05d20a92df18
    Signed-off-by: Jun-Fei Cherng <jfcherng@realtek.com>

--- test1.c
+++
@@ -1,9 +0,0 @@
-void test_func(int *a)
-{
-  // robert: add a comment to test git/repo/gerrit
-
-  *a+=5;
-  printf ("Added test1.c\n");
-
-  return 0;
-}
--- yyy.img.sig
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0284bebfbd41db76a89d34f0fb1c3002bfd4735cebfb8dd18ee407025e678f5f
-size 310
