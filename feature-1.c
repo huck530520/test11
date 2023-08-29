f6c3b80c7da8c7d2ef2ac46c640770f224dd8a67
Rey Cheng <reycheng@realtek.com>
2023-08-29 13:49:42 +0800

    update defects position
    Signed-off-by: Rey Cheng <reycheng@realtek.com>
    Change-Id: Ib7ee3d910575c92cae54d7e8ec08dff774f2a860

--- feature-1.c
+++ feature-1.c
@@ -25,10 +25,9 @@ int main() {
 
 
 
-    int *p2 = malloc(sizeof(int));
-    *p2 = 4;
-
-    *p2 = 100;
+    //int *p2 = malloc(sizeof(int));
+    //*p2 = 4;
+    //*p2 = 100;
 
     out_of_bound_access(2048);
     printf("trigger by comment added on gerrit-dup-4\n");
