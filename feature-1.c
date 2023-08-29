1361fee91fe6b1e9c21638a1456562424c4ea6a1
Rey Cheng <reycheng@realtek.com>
2023-08-29 16:54:23 +0800

    update
    Signed-off-by: Rey Cheng <reycheng@realtek.com>
    Change-Id: Ie15187eebc2efab2f21e1b4f5598b0cc4ffc286f

--- feature-1.c
+++ feature-1.c
@@ -17,9 +17,19 @@ void out_of_bound_access(int data_len) {
 }
 
 int main() {
-    int *p2 = malloc(sizeof(int));
-    *p2 = 4;
-    *p2 = 100;
+
+
+
+
+
+
+
+
+
+
+    //int *p2 = malloc(sizeof(int));
+    //*p2 = 4;
+    //*p2 = 100;
 
     out_of_bound_access(2048);
     printf("trigger by comment added on gerrit-dup-4\n");
