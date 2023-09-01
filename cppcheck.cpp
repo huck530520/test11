d2b5d77ada3d45d3d65ae8f8c24aaad405e0b5fc
Rey Cheng <reycheng@realtek.com>
2023-09-01 16:44:56 +0800

    change defects
    Signed-off-by: Rey Cheng <reycheng@realtek.com>
    Change-Id: Ie9740d8c0579723ed24531de7bf2e7564713566e

--- cppcheck.cpp
+++ cppcheck.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     std::cout << "Hello World!";
-    int *p;
-    *p = 5;
+    //int *p;
+    //*p = 5;
 
-    //char *ptr = malloc(10);
-    //std::cout << ptr[10];
+    char *ptr = malloc(10);
+    std::cout << ptr[10];
 
     return 0;
 }
