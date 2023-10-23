9f0fdaf4ba9ee6c29c4dac7615462e2581d5f04d
Rey Cheng <reycheng@realtek.com>
2023-10-23 13:43:19 +0800

    add submodule
    Signed-off-by: Rey Cheng <reycheng@realtek.com>
    Change-Id: Id9718b0512c1cf63198d77eb0c34e7bca6b584e8

---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+import subprocess as sb
+
+#curl "https://ctcsoc-jks.rtkbf.com/jenkins/buildByToken/build?job=test/test-pipelineascode-urf-triggered-ftp&token=mytriggertoken"
+cmds = ['curl', '-X', 'POST', '--url', \
+        'https://ctcsoc-jks.rtkbf.com/jenkins/buildByToken/buildWithParameters/build?job=test/test-pipelineascode-urf-triggered-ftp&token=mytriggertoken&RELEASE_NAME=RDC_Demo-Internal-611-2023_1019_1711&SMSURF_ID=166659', '-o', 'output.html']
+cmdShell = sb.Popen(cmds, stdout=sb.PIPE)
+while True:
+    line = cmdShell.stdout.readline()
+    try:
+        print(line.decode('utf-8').strip(), flush=True)
+    except:
+        print(line.strip(), flush=True)
+    if not line:
+        break
+cmdShell.communicate()
