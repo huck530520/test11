aa02f60f399b2b694904174ffbd4465db0445d9d
Rey Cheng <reycheng@realtek.com>
2023-10-24 10:56:33 +0800

    test polling
    Signed-off-by: Rey Cheng <reycheng@realtek.com>

--- feature-1.c
+++ feature-1.c
@@ -27,6 +27,7 @@ int main() {
     // patchset 3
     // patchset 4
     // patchset 5
+    // patchset 6
     out_of_bound_access(2048);
     printf("trigger by comment added on gerrit-dup-4\n");
 
