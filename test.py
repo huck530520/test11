da3e34bcf8928b879690921b08d649c73462b705
Rey Cheng <reycheng@realtek.com>
2024-03-12 16:54:07 +0800

    test message
    Change-Id: Id8edbadd6623e7099eebbe3659eb00339f719fdf
    Signed-off-by: Rey Cheng <reycheng@realtek.com>

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import os
 import subprocess as sb
 
+# new comment
+
 #curl "https://ctcsoc-jks.rtkbf.com/jenkins/buildByToken/build?job=test/test-pipelineascode-urf-triggered-ftp&token=mytriggertoken"
 cmds = ['curl', '-X', 'POST', '--url', \
         'https://ctcsoc-jks.rtkbf.com/jenkins/buildByToken/buildWithParameters/build?job=test/test-pipelineascode-urf-triggered-ftp&token=mytriggertoken&RELEASE_NAME=RDC_Demo-Internal-611-2023_1019_1711&SMSURF_ID=166659', '-o', 'output.html']
