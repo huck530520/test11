acd0507d8290e2b1988210596ae6d3ec5a456b54
Rey Cheng <reycheng@realtek.com>
2025-07-17 17:16:09 +0800

    RealGPT review:     The variable 'src' was declared without initialization, leading to an uninitialized pointer being used in the 'memcpy' call. This was fixed by allocating memory for 'src' using 'malloc(1024)' to match the size used in the memcpy operation. This ensures 'src' points to a valid memory address before being used, eliminating the undefined behavior flagged by Coverity.
    http://192.168.56.101:8080/job/pipeline-as-code/job/test-pipeline-framework/6220/
    
    Change-Id: If4032c3714c7c4e0a8b8f3dd36a3fa46ebb09737
    Signed-off-by: Rey Cheng <reycheng@realtek.com>

--- dirb/test.c
+++ dirb/test.c
@@ -1,6 +1,30 @@
-int main() {
+<pre>    int main() {
+</pre>
+<table summary="events" bgcolor=#e0e0e0>
+<tr><td><font color="red">
+<a name="error:0"></a>
+<!-- merge-key a2dbf45e8d682870f7dd0db9de1bad05 -->
+<b>(1) Event var_decl</b>: </font></td>
+<td><font color="red">Declaring variable &quot;src&quot; without initializer.</font></td>
+</tr>
+<tr><td><font color="red">Also see events: </font></td>
+<td><font color="red">[<a href="#error:1">uninit_use_in_call</a>]</font></td></tr>
+</table>
+<pre>        char *src = malloc(1024); // Initialize src with allocated memory
     int *p = malloc(100);
+</pre>
+<table summary="events" bgcolor=#e0e0e0>
+<tr><td><font color="red">
+<a name="error:1"></a>
+<a name="error"></a>
+<!-- merge-key a2dbf45e8d682870f7dd0db9de1bad05 -->
+<b>(2) Event uninit_use_in_call</b>: </font></td>
+<td><font color="red">Using uninitialized value &quot;src&quot; when calling &quot;memcpy&quot;.</font></td>
+</tr>
+<tr><td><font color="red">Also see events: </font></td>
+<td><font color="red">[<a href="#error:0">var_decl</a>]</font></td></tr>
+</table>
+<pre>        memcpy(p, src, 1024);
     int *q = 4;
-
-    return 0;
 }
+</pre>
