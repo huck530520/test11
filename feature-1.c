4bf766fe3fc62210efdbf5e48bdf3fa3f0bfe75a
Rey Cheng <reycheng@realtek.com>
2025-08-05 14:01:23 +0800

    add func()

--- feature-1.c
+++ feature-1.c
@@ -1,7 +1,24 @@
+void func() {
+    int a;
+    if (a > 0) {
+        printf("test 1\n");
+	return;
+    }
+    else {
+	printf("test 2\n");
+	return;
+    }
+
+    a = 999;
+    return;
+}
+
 int main() {
     int *p;
     *p = 5;
     free(p);
     *p = 0;
+    func();
+
     return 0;
 }
